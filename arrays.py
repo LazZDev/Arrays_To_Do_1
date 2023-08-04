6c853ccee43d4cdf8ae18ece14ae988adcb527f5
lazaromasot <lazz84@hotmail.com>
2023-08-03 23:29:06 -0400

    Added the instructions to each exercise

--- arrays.py
+++ arrays.py
@@ -1,4 +1,5 @@
 # Shuffle
+# In JavaScript, the Array object has numerous useful methods. It does not, however, contain a method that will randomize the order of an array’s elements. Let’s create shuffle(arr), to efficiently shuffle a given array’s values. Work in-place, naturally. Do you need to return anything from your function?
 import random
 
 
@@ -14,6 +15,7 @@ print(my_array)
 
 
 # Skyline Heights
+# Lovely Burbank has a breathtaking view of the Los Angeles skyline. Let’s say you are given an array with heights of consecutive buildings, starting closest to you and extending away. Array [-1,7,3] would represent three buildings: first is actually out of view below street level, behind it is second at 7 stories high, third is 3 stories high (hidden behind the 7-story). You are situated at street level. Return array containing heights of buildings you can see, in order. Given [-1,1,1,7,3] return [1,7]. Given [0,4] return [4]. As always with challenges, do not use built-in array functions such as unshift().
 def skyline_heights(arr):
     visible_buildings = []
     max_height = 0
@@ -32,6 +34,7 @@ print(skyline_heights(buildings2))
 
 
 # Zip It
+# Create a standalone function that accepts two arrays and combines their values sequentially into a new array. Extra values from either array should be included afterward. Given [4,15,100] and [10,20,30,40], return new array containing [4,10,15,20,30,40,100].
 def zip_arrays(arr1, arr2):
     result = []
     length = max(len(arr1), len(arr2))
